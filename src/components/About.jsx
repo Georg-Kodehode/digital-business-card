05a8e40ed17f33e1d7ddd2ea4b1929da264fcf0f
Georg kodehode <georg.kodehode@gmail.com>
2023-01-26 11:02:37 +0100

    add about and interests components

---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <>
+      <h4 className="main-section-titles">About</h4>
+      <p className="main-section-paragraphs">
+        I am a full stack developer who likes to learn how to use new
+        technologies. When I work, I like to do tasks properly, and reach set
+        goals.
+      </p>
+    </>
+  );
+}
---
+++ src/components/Interests.jsx
@@ -0,0 +1,11 @@
+export default function Interests() {
+  return (
+    <>
+      <h4 className="main-section-titles">Interests</h4>
+      <p className="main-section-paragraphs">
+        Programming. Technology. Movies and TV. Board games. Video games. Books.
+        Football. Travel.
+      </p>
+    </>
+  );
+}
